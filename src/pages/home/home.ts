b92192b6047c733386551161be60b944e83504b2
MudauE <enockmudau2@gmail.com>
2018-06-27 09:28:57 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,77 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  scoreA = 0;
+  scoreB = 0;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  Click(){
+    this.scoreA +=3;
+    console.log(this.scoreA)
+    
+   
+  }
+
+  ClickOne(){
+
+    this.scoreA +=2;
+    console.log(this.scoreA)
+    
+    
 }
+
+ClickTwo(){
+  console.log(this.scoreA)
+  this.scoreA +=1;
+  
+}
+  ClickThree(){
+    this.scoreB +=3;
+    console.log(this.scoreB)
+    
+   
+ } 
+ ClickFour(){
+  this.scoreB +=2;
+  console.log(this.scoreB)
+ 
+  
+ }
+  ClickFive(){
+    this.scoreB +=1;
+    console.log(this.scoreB)
+    
+   
+  }
+
+  ClickSix(){
+  this.scoreA = 0;
+
+  this.scoreB = 0;
+    
+
+  }  
+
+}
+
+
+
+
+
+  
+   
+  
+
+
+
+
+ 
+ 
+
+
+
+
+
